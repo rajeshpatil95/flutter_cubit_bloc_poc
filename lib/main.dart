91848c009e276fc97cc31ade9d8b1957e367d8fe
Rajesh Patil <rajeshkpatil1995@gmail.com>
2022-11-11 18:04:49 +0530

    feature: main commands added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// main.dart should not be used, use either main_dev.dart or main_prod.dart which invokes main_default.dart
+// main.dart should not be used, use folder either main->main_dev.dart or main->main_prod.dart which invokes main_default.dart
 //flutter run -d 86S7N18426004686 --flavor Dev -t lib/main/main_dev.dart -- To run dev version
 //flutter build apk --release --flavor dev -t lib/main/main_dev.dart     -- To build dev release apk
 //flutter build appbundle --flavor production -t lib/main/main_prod.dart -- To build prod release apk
